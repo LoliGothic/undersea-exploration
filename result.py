c80846d614276413306a27422f5d1e644ee052c4
haru <haruhito.matsu@gmail.com>
2022-07-18 00:08:16 +0900

    Create result.py

--- result.py
+++ result.py
@@ -1,8 +1,9 @@
 import pygame
 
 # pointPlayer1とかはplayerそれぞれのポイント
-def result(pointPlayer1, pointPlayer2, pointPlayer3, pointPayer4):
+def result(pointPlayer1, pointPlayer2, pointPlayer3, pointPlayer4):
   pygame.init()
+  
 
   # 画面サイズ
   screen = pygame.display.set_mode((400, 400))
@@ -21,11 +22,27 @@ def result(pointPlayer1, pointPlayer2, pointPlayer3, pointPayer4):
       # 文字の大きさを指定
       font = pygame.font.Font(None, 45)
 
-      # 文字の色を指定
-      text = font.render("hello", True, (255,255,255))
+      # 出力する文字、文字の色を指定
+      text = font.render("player1", True, (255,255,255))
+      text2 = font.render("player2", True, (255,255,255))
+      text3 = font.render("player3", True, (255,255,255))
+      text4 = font.render("player4", True, (255,255,255))
+      score = font.render(str(pointPlayer1), True, (255,255,255))
+      score2 = font.render(str(pointPlayer2), True, (255,255,255))
+      score3 = font.render(str(pointPlayer3), True, (255,255,255))
+      score4 = font.render(str(pointPlayer4), True, (255,255,255))
 
-      # 文字の座標
-      screen.blit((text, [40, 40]))
+      # 文字の出力、座標
+      screen.blit(text, (40, 40))
+      screen.blit(text2, (40, 100))
+      screen.blit(text3, (40, 160))
+      screen.blit(text4, (40, 220))
+      screen.blit(score, (200, 40))
+      screen.blit(score2, (200, 100))
+      screen.blit(score3, (200, 160))
+      screen.blit(score4, (200, 220))
 
       # 画面を再描画
       pygame.display.update()
+#resultの実行
+# result(100, 120, 60, 40)
